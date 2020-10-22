a9efecf8459bf2c8f1a38e27c861f09a6b965a32
keithbooher <keith.booher@yahoo.com>
2020-10-22 16:38:22 -0500

    remove gitignore files

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+
+
+  return (
+    <div className="App App-header">
+
+      {/* buttons (next and reset) */}
+
+      {/* invisible table to hold roomba */}
+
+      {/* output table */}
+
+    </div>
+  );
+}
+
+export default App;
